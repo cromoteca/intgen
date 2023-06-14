84d7b886fec38689543c56902e016228b42eea6a
Luciano Vernaschi <luciano@vaadin.com>
2023-06-14 18:57:18 +0200

    Add header text

--- src/main/java/com/vaadin/intgen/components/FreeText.java
+++ src/main/java/com/vaadin/intgen/components/FreeText.java
@@ -6,13 +6,15 @@ import javax.swing.JLabel;
 
 public class FreeText implements ComponentGenerator<JLabel> {
 
+  public static final String[] TAGS = new String[] {"h1", "h2", "h3", "p", "p", "p", "p", "pre"};
+
   @Override
   public JLabel generate() {
+    var tag = Intgen.pickOne(TAGS);
     var text =
         String.format(
-            "<html><body style='width: %dpx'>%s</body></html>",
-            Intgen.RANDOM.nextInt(100, 250), Intgen.words(4, 15));
-    var label = new JLabel(text);
-    return label;
+            "<html><body style='width: %dpx'><%s>%s</%s></body></html>",
+            Intgen.RANDOM.nextInt(100, 250), tag, Intgen.words(4, 15), tag);
+    return new JLabel(text);
   }
 }
