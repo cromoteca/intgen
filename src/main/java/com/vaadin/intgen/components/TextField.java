1cca5dcf9f5f950d29ccccb93ed12a16fcd183b8
Luciano Vernaschi <luciano@vaadin.com>
2023-06-20 08:27:39 +0200

    Add Data Faker to generate text

--- src/main/java/com/vaadin/intgen/components/TextField.java
+++ src/main/java/com/vaadin/intgen/components/TextField.java
@@ -3,11 +3,40 @@ package com.vaadin.intgen.components;
 import com.vaadin.intgen.ComponentGenerator;
 import com.vaadin.intgen.Intgen;
 import java.awt.Dimension;
+import java.util.function.Supplier;
 import javax.swing.JTextField;
+import net.datafaker.Faker;
 
 public class TextField implements ComponentGenerator<JTextField> {
   private final int minWidth;
   private final int maxWidth;
+  private final Faker faker = new Faker();
+  private final Supplier<String>[] text =
+      new Supplier[] {
+        () -> faker.address().city(),
+        () -> faker.address().country(),
+        () -> faker.address().streetName(),
+        () -> faker.address().streetAddress(),
+        () -> faker.address().zipCode(),
+        () -> faker.phoneNumber().cellPhone(),
+        () -> faker.phoneNumber().phoneNumber(),
+        () -> faker.internet().emailAddress(),
+        () -> faker.internet().domainName(),
+        () -> faker.internet().url(),
+        () -> faker.lorem().characters(),
+        () -> faker.lorem().paragraph(),
+        () -> faker.lorem().sentence(),
+        () -> faker.lorem().word(),
+        () -> faker.name().fullName(),
+        () -> faker.name().name(),
+        () -> faker.name().username(),
+        () -> faker.name().firstName(),
+        () -> faker.name().lastName(),
+        () -> faker.name().prefix(),
+        () -> faker.name().suffix(),
+        () -> "••••••••••••",
+        () -> "●●●●●●●●●●●●"
+      };
 
   public TextField() {
     this(100, 250);
@@ -24,7 +53,12 @@ public class TextField implements ComponentGenerator<JTextField> {
     textField.setPreferredSize(
         new Dimension(
             Intgen.RANDOM.nextInt(minWidth, maxWidth), textField.getPreferredSize().height));
-    textField.setText(Intgen.words(0, 6));
+
+    if (Intgen.RANDOM.nextBoolean()) {
+      textField.setText(Intgen.pickOne(text).get());
+    } else {
+      textField.setText(Intgen.words(0, 6));
+    }
 
     if (Intgen.RANDOM.nextDouble() > 0.8) {
       textField.setEditable(false);
